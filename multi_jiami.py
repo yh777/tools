5c229f463fabd1184a7fc525a7a3bf410d3da9e7
yh777 <21872017@qq.com>
2018-04-17 15:22:24 +0800

    添加 multi_jiami.py脚本(满贯捕鱼客户端,csv多进程加密)

---
+++ multi_jiami.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# @Time    : 2018/4/17 11:22
+# @Author  : jiangysh
+"""
+渠道csv加密(多进程),选项提供所有需要加密的渠道号,脚本自行区分Android或iPhone
+使用方法: multi_jiami.py qdNumber qdNumber ......
+"""
+
+import os,sys,time
+from multiprocessing import Process
+
+basicDir = '/opt/web/soonyo_yx/game_download/by'    # 渠道资源所在的根目录
+shellScriptAn = '/root/sh/android_jiami_md5.sh'     # 安桌加密脚本
+shellScriptIp = '/root/sh/iphone_jiami_md5.sh'      # iphone加密脚本
+
+def whatSys(qdNum):                                 # 判断渠道是啥系统
+    return os.listdir(basicDir + '/' + qdNum)[0]
+
+def run(qdNum):                       # 加密函数
+    sysName = whatSys(qdNum)
+    if sysName == 'Android':
+        os.system(shellScriptAn + ' ' + qdNum)
+    elif sysName == 'iPhone':
+        os.system(shellScriptIp + ' ' + qdNum)
+
+if len(sys.argv) > 1:
+    qdNums = sys.argv[1:]         # 要加密的渠道
+else:
+    print('使用方法: multi_jiami.py qdNumber qdNumber ......')
+    sys.exit()
+
+processes = []
+for qdNum in qdNums:
+    p = Process(target=run,args=(qdNum,))
+    p.start()
+    processes.append(p)
+    time.sleep(1)
+
+for p in processes:
+    p.join()
